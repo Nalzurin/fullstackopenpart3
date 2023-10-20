f082fce2a6baf4736d9d2e1233dd50ff50e920ce
Nalzurin <alexnormandi@ukr.net>
2023-10-21 01:39:37 +0300

    Add task 3.3

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,44 +1,61 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hi!, please go to <a href="/api/persons">here</a></h1>')
-})
+app.get("/", (request, response) => {
+  response.send('<h1>Hi!, please go to <a href="/api/persons">here</a></h1>');
+});
 
-app.get('/info', (request, response) => {
-    const d = new Date();
-    response.send(`<h1>Hi!</h1><p>Phonebook has ${persons.length} entries</p><p>${d.toUTCString()}</p>`)
-  })
-  
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.filter((person) => person.id === id);
+  if(person)
+  {
+    response.json(person);
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
+  }else
+  {
+    response.status(404).end();
+  }
+});
 
-const PORT = 3001
+
+app.get("/info", (request, response) => {
+  const d = new Date();
+  response.send(
+    `<h1>Hi!</h1><p>Phonebook has ${
+      persons.length
+    } entries</p><p>${d.toUTCString()}</p>`
+  );
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
