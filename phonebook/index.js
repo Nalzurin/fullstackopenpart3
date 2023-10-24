73c356b151179f5c6f488e091d8f934535cc9306
Nalzurin <alexnormandi@ukr.net>
2023-10-24 18:00:28 +0300

    Add tasks 3.9-21

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,92 +1,117 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-morgan.token('body', function (req, res) { if(req.method === "POST")return JSON.stringify(req.body) })
+const cors = require("cors");
+morgan.token("body", function (req, res) {
+  if (req.method === "POST") return JSON.stringify(req.body);
+});
 const app = express();
+app.use(cors());
 app.use(express.json());
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body", "default"));
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+app.use(express.static("dist"));
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :body",
+    "default"
+  )
+);
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  } 
+
+  next(error);
+};
+
+const Person = require("./models/person");
 
 app.get("/", (request, response) => {
-  response.send('<h1>Hi!, please go to <a href="/api/persons">here</a></h1>');
+  response.sendFile(__dirname + "/dist/index.html");
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.filter((person) => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get("/api", (request, response) => {
+  response.send('<p>Hi! Please go to <a href="/api/persons">here.<a/></p>');
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id !== id);
-  if (person) {
-    response.status(204).end();
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/persons", (request, response) => {
+  Person.find({}).then((result) => {
+    response.json(result);
+  });
+});
+
+app.post("/api/persons", (request, response, next) => {
+  console.log("Adding person", request.body);
+
+  const person = new Person({
+    name: request.body.name,
+    number: request.body.number,
+  });
+
+  person
+    .save()
+    .then((result) => {
+      console.log(`Added ${person.name} number ${person.phone} to phonebook`);
+      response.status(201).json(person);
+    })
+    .catch((error) => next(error));
+});
+
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body;
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/info", (request, response) => {
   const d = new Date();
-  response.send(
-    `<h1>Hi!</h1><p>Phonebook has ${
-      persons.length
-    } entries</p><p>${d.toUTCString()}</p>`
-  );
+  Person.count({}).then((count) => {
+    response.send(
+      `<h1>Hi!</h1><p>Phonebook has ${count} entries</p><p>${d.toUTCString()}</p>`
+    );
+  });
 });
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
-});
+app.use(unknownEndpoint);
+// this has to be the last loaded middleware.
+app.use(errorHandler);
 
-app.post("/api/persons", (request, response) => {
-  if (!request.body || !request.body.name | !request.body.number) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
-  if (
-    persons.find((person) => {
-      if (person.name === request.body.name) return person;
-    })
-  ) {
-    return response.status(400).json({
-      error: "Name must be unique",
-    });
-  }
-  const person = request.body;
-  person.id = Math.floor(Math.random() * 5000000);
-  persons.concat(person);
-  console.log(person);
-  response.status(201).json(person);
-});
-
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ phonebook/mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+//wc7FxhaxEvv2fvo6
+if (process.argv.length < 3) {
+  console.log("give password as an argument");
+  process.exit(1);
+}
+
+const password = encodeURIComponent(process.argv[2]);
+
+const url = `mongodb+srv://alexnormandi:${password}@nalzurin.qd9cicy.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+if (process.argv.length === 3) {
+  console.log("phonebook:");
+  Person.find({})
+    .then((result) => {
+      result.forEach((person) => {
+        console.log(`${person.name} ${person.phone}`);
+      });
+      mongoose.connection.close();
+      process.exit(1);
+    })
+    .catch(() => {
+      mongoose.connection.close();
+      process.exit(1);
+    });
+}
+
+if (process.argv.length === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    phone: process.argv[4],
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${person.name} number ${person.phone} to phonebook`);
+    mongoose.connection.close();
+    process.exit(1);
+  });
+}
